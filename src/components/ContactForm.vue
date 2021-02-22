50d6cf8d96ce4e32a823bef6cb1e69b1dec66b2e
Travis Reynolds <travis@cirruscreative.co.uk>
2021-02-22 15:24:37 +0000

    Add demo contact form

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,71 @@
+<template>
+  <form
+    class="form"
+    @submit.prevent="handleSubmit">
+    <div class="field">
+      <div class="control">
+        <label
+          for="Name"
+          class="label">
+          <input
+            id="Name"
+            v-model="form.Name"
+            type="text"
+            class="input"
+            name="Name"
+            placeholder="Enter your name">
+        </label>
+      </div>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'ContactForm',
+  data: () => ({
+    recaptcha: '',
+    form: {},
+    state: {
+      loading: false,
+      success: false,
+      error: false
+    }
+  }),
+  methods: {
+    encode (data) {
+      return Object.keys(data)
+        .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[ key ]))
+        .join('&')
+    },
+    async handleSubmit () {
+      try {
+        this.state = { loading: true, error: false, success: false }
+
+        await this.$recaptchaLoaded()
+        const token = await this.$recaptcha('contact')
+
+        const res = await fetch('/', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+          body: this.encode({
+            'form-name': 'Contact Form',
+            'g-recaptcha-response': token,
+            ...this.form
+          })
+        })
+
+        if (!res.ok) throw new Error('Something went wrong. Please try again...')
+        this.state = { loading: false, success: true, error: false }
+      } catch (error) {
+        console.error(error)
+        this.state = {
+          loading: false,
+          success: false,
+          error: error.message
+        }
+      }
+    }
+  }
+}
+</script>
